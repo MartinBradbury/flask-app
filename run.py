f6a5c33fe1060bd9650905a197bcb2445287b38d
Martin Bradbury <martinbradbury@btinternet.com>
2024-02-06 18:23:45 +0000

    linked pages together with route and added contact page

--- run.py
+++ run.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+
+
 if __name__ == "__main__":
     app.run(
         host = os.environ.get("IP", "0.0.0.0"),
